7fce746f68fe22b22e5c01f12e822063dd543646
ara8586 <ara@mailfa.com>
2017-02-10 13:49:54 +0330

    Update banhammer-fa.lua

--- plugins/banhammer-fa.lua
+++ plugins/banhammer-fa.lua
@@ -851,8 +851,5 @@ return {
 pre_process = pre_process
 }
 
--- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
+-- by @mr_ahmadix
+-- sp @suport_arabot
